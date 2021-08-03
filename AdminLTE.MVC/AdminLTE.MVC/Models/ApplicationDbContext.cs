ca71db207d28b11cdc998338920332607366e680
Noorudinz <noorudeens@yahoo.in>
2021-08-03 21:56:57 +0530

    Changes from mukeshcode blog

--- AdminLTE.MVC/AdminLTE.MVC/Models/ApplicationDbContext.cs
+++ AdminLTE.MVC/AdminLTE.MVC/Models/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AdminLTE.MVC.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,14 @@ namespace AdminLTE.MVC.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Seed();
+        }
+
     }
 }
