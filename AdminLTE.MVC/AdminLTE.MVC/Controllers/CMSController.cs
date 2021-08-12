c0cb6a8b69d18fad7aa35951f9e17da7553ee923
Noorudinz <noorudeens@yahoo.in>
2021-08-12 22:25:11 +0530

    CMS text editor summernote added

---
+++ AdminLTE.MVC/AdminLTE.MVC/Controllers/CMSController.cs
@@ -0,0 +1,29 @@
+﻿using AdminLTE.MVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTE.MVC.Controllers
+{
+    public class CMSController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CMSController(ApplicationDbContext context)
+        {            
+            _context = context;           
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HomeBanner()
+        {           
+            var banner = _context.Contents.SingleOrDefault(x => x.ContentType == "HOME_MAIN_BANNER" && x.IsActive == true);
+            return View(banner);
+        }
+    }
+}
