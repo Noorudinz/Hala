2ac1ddf33b4400a2ab819f37f1acef695933d7e5
Noorudinz <noorudeens@yahoo.in>
2021-08-13 21:30:47 +0530

    CMS content function modified

--- AdminLTE.MVC/AdminLTE.MVC/Models/HomeBannerRTB.cs
+++ AdminLTE.MVC/AdminLTE.MVC/Models/HomeBannerRTB.cs
@@ -12,7 +12,11 @@ namespace AdminLTE.MVC.Models
         [Key]
         public int ContentId { get; set; }
         public string ContentType { get; set; }
+        public string ContentTitle { get; set; }
         public string ContentDetails { get; set; }
+        public int Orders { get; set; }
         public bool IsActive { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
     }
 }
