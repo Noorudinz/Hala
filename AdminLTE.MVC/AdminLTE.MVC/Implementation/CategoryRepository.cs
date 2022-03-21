b47685017b9ad177dfbe488ec2e60673d6153282
Noorudins <noorudins@cloudix.io>
2022-03-21 08:57:52 +0530

    Category repo and controller added.

---
+++ AdminLTE.MVC/AdminLTE.MVC/Implementation/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using AdminLTE.MVC.Data;
+using AdminLTE.MVC.Models;
+using AdminLTE.MVC.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTE.MVC.Implementation
+{
+    public class CategoryRepository : ICategory
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public Category AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllCategorys()
+        {
+            return _context.Category.ToList();
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            return _context.Category.Where(w => w.CategoryId == categoryId).FirstOrDefault();
+        }
+
+        public Category UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdminLTE.MVC/AdminLTE.MVC/Repository/ICategory.cs
@@ -0,0 +1,17 @@
+﻿using AdminLTE.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTE.MVC.Repository
+{
+    public interface ICategory
+    {
+        List<Category> GetAllCategorys();
+        Category GetCategoryById(int categoryId);
+        Category AddCategory(Category category);
+        Category UpdateCategory(Category category);
+        void DeleteCategory(int categoryId);
+    }
+}
